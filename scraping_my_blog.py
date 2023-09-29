b34ae06021b9a0118546131e860b6904fc92dbcc
Byeong Jun <chchdelm3@icloud.com>
2023-09-29 09:36:18 +0900

    changed post tag

--- scraping_my_blog.py
+++ scraping_my_blog.py
@@ -7,17 +7,14 @@ def scrape_blog_updates():
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, "html.parser")
 
-    # 'article-list' 클래스를 가진 섹션에서 게시물 찾기
-    articles = soup.find("section", class_="article-list").find_all("article")
+    articles = soup.find("section", class_="article-list").find_all("article")[:5]
+    posts = []
+    for article in articles:
+        title = article.find("h2", class_="article-title").text.strip()
+        date = article.find("time", class_="article-time--published").text.strip()
+        posts.append(f"* {title} - {date}")
 
-    # 상위 5개의 게시물만 가져오기.
-    for article in articles[:5]:
-        title = article.find("h2", class_="article-title").text.strip()  # 제목
-        link = article.find("a")["href"]  # 링크
-        date = article.find("time", class_="article-time--published").text.strip()  # 날짜
-
-        print(f"Title: {title}\nLink: {link}\nDate: {date}\n---")
+    return "\n".join(posts)
 
 
-# 웹 스크래핑 함수 실행
-scrape_blog_updates()
+# print(scrape_blog_updates())
